dc6d1d0a7fd337b73af3a5bfa18090b35898f08f
Caylan Larson <caylan@semireg.com>
2018-10-29 15:49:14 -0500

    Add notes on breakage

--- app/config/types.js
+++ app/config/types.js
@@ -1,6 +1,8 @@
 // @flow
 
-export const blah = 'blah';
+// Without this line, error: Uncaught SyntaxError: Unexpected Identifier, RCTFatal
+// Interestingly, it can not recover until `react-native run-ios` is re-run.
+export const BrokenTypeExportHack = 'BrokenTypeExportHack'; // eslint-disable-line
 
 export type FooState = {
   count: number
