2b0bbd10688fae95e388fa3b5443789ecf006c6e
Caylan Larson <caylan@semireg.com>
2018-10-30 13:54:30 -0500

    Lighten text color on dark bg

--- app/config/styles.js
+++ app/config/styles.js
@@ -1,12 +1,10 @@
 // @flow
 
-export const stuffs = {
-
-};
+export const stuffs = {};
 
 export const colors = {
   background: '#172754',
   brightBlue: '#2F75C0',
   header: '#F5FCFF',
-  gray: '#333333',
+  gray: '#AAAAAA',
 };
