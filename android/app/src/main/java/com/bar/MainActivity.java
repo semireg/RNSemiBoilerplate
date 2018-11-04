9b702e1cf228c58ecb4192730cd817fe798af3a6
Caylan Larson <caylan@semireg.com>
2018-11-04 13:06:53 -0600

    Add react screens

--- android/app/src/main/java/com/bar/MainActivity.java
+++ android/app/src/main/java/com/bar/MainActivity.java
@@ -1,8 +1,14 @@
 package com.bar;
 
-import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.facebook.react.ReactFragmentActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // @flow
 
+// Unsure why this is not resolving properly.
+import { useScreens } from 'react-native-screens'; // eslint-disable-line import/no-unresolved
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+useScreens();
 
 AppRegistry.registerComponent(appName, () => App);
